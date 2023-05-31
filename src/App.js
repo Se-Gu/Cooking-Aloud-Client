b91aa97ad3e74b909b024f23840e36938653040e
Serhat Gürgenyatağı <63667747+Se-Gu@users.noreply.github.com>
2023-05-31 20:34:57 +0300

    AuthProvider deleted from App.js

--- src/App.js
+++ src/App.js
@@ -4,26 +4,19 @@ import LoginPage from "./pages/LoginPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import UserProfile from "./pages/UserProfile";
-import { AuthProvider } from 'react-auth-kit'
+import { AuthProvider } from "react-auth-kit";
 
 export default function App() {
   return (
     <div>
-	
-    
       <Header />
       <BrowserRouter>
-	  <AuthProvider authType = {'cookie'}
-                  authName={'_auth'}
-                  cookieDomain={window.location.hostname}
-                  cookieSecure={window.location.protocol === "http:"}>
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/profile" element={<UserProfile />} />
         </Routes>
-		</AuthProvider>
       </BrowserRouter>
     </div>
   );
