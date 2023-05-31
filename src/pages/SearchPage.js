b66e1631d11215c469e1dad291bb860a7934b97d
Serhat Gürgenyatağı <63667747+Se-Gu@users.noreply.github.com>
2023-05-31 23:53:10 +0300

    searchpage

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,79 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  Container,
-  Typography,
-  TextField,
-  Button,
-  Grid,
-} from "@material-ui/core";
-import { useSignIn, useAuthUser, useAuthHeader, useIsAuthenticated } from "react-auth-kit";
-import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-
-const baseURL = "http://localhost:8080/api/"
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchPage = () => {
-	const signIn = useSignIn();
-	const classes = useStyles();
-	const isAuthenticated = useIsAuthenticated();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const nav = useNavigate();
-	const auth = useAuthUser();
-	const authHeader = useAuthHeader();
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const nav = useNavigate();
 
-  const fetchData = async () => {
-		const response = await axios.get(
-			baseURL + "getfavorite", 
-			{headers: {'Authorization': 'Bearer ' + auth().token,
-			'Accept' : 'application/json',
-			'Content-Type': 'application/json'}}
-		);
-		console.log(response.data);
-		}
-  
-	useEffect(() =>{
-		const response = fetchData().catch(console.error);
-
-  },[])
-
-
-	
+  const search = () => {
+    nav(`/search/${query}`);
+  };
 
   return (
-   <form action="/search" method="get">
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Search blog posts</span>
-        </label>
-        <input
-            type="text"
-            id="header-search"
-            placeholder="Search blog posts"
-            name="s" 
-        />
-        <button type="submit">Search</button>
-    </form>
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for recipes"
+      />
+      <button onClick={search}>Search</button>
+
+      {searchResults.length > 0 ? (
+        <ul>
+          {searchResults.map((recipe) => (
+            <li key={recipe.id}>{recipe.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No results found</p>
+      )}
+    </div>
   );
 };
 
 export default SearchPage;
-
-
-
